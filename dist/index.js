1bb31b0b0e61cfd7453a7c4d2661e94ae328b414
gesheng <ge@aspring.design>
2022-08-08 21:39:45 +0800

    fix module import bug

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import { Vec3 } from "Vec3";
-import { Matrix3, so3, Rotation3 } from "Matrix3";
-import { Matrix4 } from "Matrix4";
+import { Vec3 } from "./Vec3";
+import { Matrix3, so3, Rotation3 } from "./Matrix3";
+import { Matrix4 } from "./Matrix4";
 export { Vec3, Matrix3, so3, Rotation3, Matrix4 };
